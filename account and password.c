e8d43906ff4069a108e9647512d2bf9d6bb35f96
Spike3517 <104782460+Spike3517@users.noreply.github.com>
2022-05-13 20:54:20 +0800

    Add files via upload

---
+++ account and password.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int account, password;
+	printf("please enter the account:\n");
+	scanf_s("%d", &account);
+	printf("please enter the secret:\n");
+	scanf_s("%d", &password);
+	if (account == 123 && password == 123)
+		printf("welcome!");
+	if (account != 123 && password != 123)
+		printf("sorry your account or secret is error\n");
+	return 0;
+}
